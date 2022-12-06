5279ff0617b04f21e21f611704c1d01b35981a1c
worldduck <tonyskh0803@gmail.com>
2022-12-06 16:41:49 +0900

    20221206

--- Stealth/Assets/02.1. Test scripts/Guard.cs
+++ Stealth/Assets/02.1. Test scripts/Guard.cs
@@ -8,12 +8,25 @@ public class Guard : MonoBehaviour
     public float waitTime = 2f;
     public float turnSpeed = 180;
 
+    public Light spotlight;
+    public float viewDistance;
+    public LayerMask viewMask;
+    private float viewAngle;
+
     public Transform pathHolder;
+    private Transform player; // 플레이어의 Transform
+    private Color originalSpotlightColor; // 스포트라이트의 기본색
+
     private Animator animator;
 
     private void Start()
     {
+        viewAngle = spotlight.spotAngle;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        originalSpotlightColor = spotlight.color;
+
         animator = GetComponent<Animator>();
+
         Vector3[] waypoints = new Vector3[pathHolder.childCount];
         for (int i = 0; i < waypoints.Length; i++)
         {
@@ -22,6 +35,38 @@ public class Guard : MonoBehaviour
         }
 
         StartCoroutine(FollowPath(waypoints));
+    }
+
+    private void Update()
+    {
+        if (CanSeePlayer())
+        {
+            spotlight.color = Color.red;
+        }
+        else
+        {
+            spotlight.color = originalSpotlightColor;
+        }
+    }
+
+    bool CanSeePlayer()
+    {
+        // 플레이어가 시야 거리 반경 안에 있다면
+        if (Vector3.Distance(transform.position, player.position) < viewDistance)
+        {
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            float angleBetweenGuardAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+            // 플레이어가 뷰 앵글 안에 있다면
+            if (angleBetweenGuardAndPlayer < viewAngle / 2f)
+            {
+                // 플레이어가 장애물에 가려지지 않았다면
+                if (!Physics.Linecast (transform.position, player.position, viewMask))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     IEnumerator FollowPath(Vector3 [] waypoints)
@@ -40,6 +85,7 @@ public class Guard : MonoBehaviour
             {
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                 targetwaypoint = waypoints[targetWaypointIndex];
+
                 animator.SetBool("isMove", false);
                 yield return new WaitForSeconds (waitTime);
                 yield return StartCoroutine(TurnToFace(targetwaypoint));
@@ -73,5 +119,9 @@ public class Guard : MonoBehaviour
             previousPosition = waypoint.position;
         }
         Gizmos.DrawLine(previousPosition, startPosition);
+
+        // 시야 거리 빨간선으로 표시
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
     }
 }
